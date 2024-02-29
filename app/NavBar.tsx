0babee88e49d1410372b928518d6c72b17845fd1
Daniel Chen <daniel.chen@aosgrp.us>
2024-02-29 21:45:36 +1100

    style nav link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
@@ -7,6 +11,8 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
+  const currentPath = usePathname();
 
   return (
     <nav className="flex space-x-6 items-center px-5 h-14 border-b mb-5">
@@ -17,7 +23,11 @@ const NavBar = () => {
         {links.map((link) => (
           <li
             key={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
