01c27bf14643ee20c0d6ec0b35ec6847e5337226
Daniel Chen <daniel.chen@aosgrp.us>
2024-03-02 21:38:50 +1100

    set up radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const Issues = () => {
-  return <div>Issues</div>;
+  return (
+    <div>
+      <Button>Add issue</Button>
+    </div>
+  );
 };
 
 export default Issues;
