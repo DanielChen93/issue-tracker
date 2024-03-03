b724b15eefff1546d27ff0eac474f569e04849eb
Daniel Chen <daniel.chen@aosgrp.us>
2024-03-03 15:11:15 +1100

    add markdown for issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
 import { Card, Flex, Heading } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import MarkDown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -19,7 +20,9 @@ const IssueDetailPage = async ({ params }: Props) => {
         <IssueStatusBadge status={issue.status} />
         <div>{issue.createdAt.toDateString()}</div>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose" mt="5">
+        <MarkDown>{issue.description}</MarkDown>
+      </Card>
     </div>
   );
 };
