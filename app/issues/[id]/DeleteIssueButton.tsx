b11df7bf58a3bf70a52608a15ef7acb34b19aec8
Daniel Chen <daniel.chen@aosgrp.us>
2024-03-11 17:14:02 +1100

    Add delete issue experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -9,13 +10,16 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
 
   const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
 
   const deleteIssue = async () => {
     try {
+      setIsDeleting(true);
       await axios.delete("/api/issues/" + issueId);
       router.push("/issues");
       router.refresh();
     } catch (error) {
+      setIsDeleting(false);
       setError(true);
     }
   };
@@ -24,7 +28,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete</Button>
+          <Button color="red" disabled={isDeleting}>
+            Delete
+            {isDeleting && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Delete Issue</AlertDialog.Title>
