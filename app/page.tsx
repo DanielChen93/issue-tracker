7b5d40ca53a11b0e81ee072f85e31371b83ccc6c
Daniel Chen <daniel.chen@aosgrp.us>
2024-03-18 17:22:00 +1100

    Add IssueSummary

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import { Flex } from "@radix-ui/themes";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await (
+    await prisma.issue.findMany({ where: { status: "OPEN" } })
+  ).length;
+  const in_progress = await (
+    await prisma.issue.findMany({ where: { status: "IN_PROGRESS" } })
+  ).length;
+  const closed = await (
+    await prisma.issue.findMany({ where: { status: "CLOSED" } })
+  ).length;
+
+  return (
+    <Flex align="start" justify="between">
+      {/* <LatestIssues /> */}
+      <IssueSummary open={open} in_progress={in_progress} closed={closed} />
+    </Flex>
+  );
 }
